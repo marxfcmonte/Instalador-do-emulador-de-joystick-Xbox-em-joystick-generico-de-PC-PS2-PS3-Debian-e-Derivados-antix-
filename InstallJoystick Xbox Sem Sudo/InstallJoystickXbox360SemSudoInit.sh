7fa38ac7a3cc95bb57e4b6c3e6c7247c4cef3eb9
Marx Fabyan Carvalho do Monte <34894112+marxfcmonte@users.noreply.github.com>
2025-05-31 09:10:18 -0300

    Versão 1.8.1
    Com os arquivos InstallJoystickXbox360SemSudo.sh e InstallJoystickXbox360SemSudoInit.sh é possível executar sem utilizar o 'sudo' ou 'su' no terminal, 
    sendo solicitado só uma vem na execução do programa.

---
+++ InstallJoystick Xbox Sem Sudo/InstallJoystickXbox360SemSudoInit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if ! [ -e "/usr/bin/dialog" ]; then
+	senha=read -p "Dialog não instalado, digite a senha (SUDO): "
+	echo $senha|sudo -S -p "" apt install -y dialog
+else
+	senha=$(dialog --title "AUTORIZAÇÃO" --passwordbox "Digite a senha (SUDO):" 8 40 --stdout)
+fi
+if [ -z "\$senha" ]; then
+	dialog --title "ERRO" --infobox "A senha (SUDO) não foi digitada." 3 40
+	sleep 3
+	clear
+	exit 1
+fi
+clear
+if ! [ -e "/usr/bin/roxterm" ]; then
+	echo -e "Roxterm não instalado e será instaladp...\n"
+	echo $senha|sudo -S -p "" apt install -y roxterm
+fi
+local="$(pwd)"
+if ! [ -e "/bin/shc" ]; then
+	echo $senha|sudo -S -p "" apt install -y shc libc6-dev
+	shc -f "$local/InstallJoystickXbox360SemSudo.sh" -o "$local/InstalljoystickXbox360SemSudo"
+	sudo bash -c "$local/InstalljoystickXbox360SemSudo"
+else
+	shc -f "$local/InstallJoystickXbox360SemSudo.sh" -o "$local/InstalljoystickXbox360SemSudo"
+	echo $senha|sudo -S -p "" bash -c "$local/InstalljoystickXbox360SemSudo"
+fi
+
+
---
+++ InstallJoystick Xbox Sem Sudo/Readme.txt
@@ -0,0 +1,4 @@
+Execute o arquivo "InstallJoystickXbox360SemSudoInit.sh" com "./InstallJoystickXbox360SemSudoInit.sh", 
+quando for solicitado a senha do "sudo", insira.
+
+O executável instalará as dependências necessárias.
